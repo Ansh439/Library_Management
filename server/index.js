22c68b861f5f5f58d425d01ff99469d11991328b
Ansh439 <gupta.ansh3150@gmail.com>
2024-12-09 21:11:55 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import Admin from "./models/Admin.js";
 
 dotenv.config();
 
@@ -10,24 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-app.post("/api/admin/login", async (req, res) => {
-  const { userId, password } = req.body;
-  try {
-    const admin = await Admin.findOne({ userId });
-    if (!admin || admin.password !== password) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-    res.status(200).json({ message: "Login successful" });
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
