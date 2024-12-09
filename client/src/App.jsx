e591c1b0f9b3672c1ce61703f170bb318a640c42
Yatendra_Rajput <yatendra1456@gmail.com>
2024-12-09 23:16:24 +0530

    user pages updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import IndexPage from "./components/IndexPage.jsx";
 import AdminLogin from "./components/AdminLogin.jsx";
 import VendorLogin from "./components/VendorLogin.jsx";
 import UserLogin from "./components/UserLogin.jsx";
+import UserPortal from "./components/UserPortal.jsx";
+import UserSignup from "./components/UserSignup.jsx";
+import MaintainUser from "./components/MaintainUser.jsx";
 
 function App() {
   return (
@@ -13,6 +16,11 @@ function App() {
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/vendor-login" element={<VendorLogin />} />
         <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/user-portal" element={<UserPortal />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+        <Route path="/user-maintainuser" element={<MaintainUser />} />
+
+
       </Routes>
     </Router>
   );
